6446456071d10521d7cbf8feb658277c947837fc
unknown <lyuboslavw@gmail.com>
2019-09-13 20:53:12 +0300

    Added Custom ORM

---
+++ Database Frameworks - Hibernate and Spring Data - 2019/03. Custom ORM/src/main/java/com/ignatov/entities/User.java
@@ -0,0 +1,75 @@
+package com.ignatov.entities;
+
+import com.ignatov.orm.annotations.Column;
+import com.ignatov.orm.annotations.Entity;
+import com.ignatov.orm.annotations.PrimaryKey;
+
+import java.sql.Date;
+
+@Entity(name = "users")
+public class User {
+    @PrimaryKey(name = "id")
+    private long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "registration_date")
+    private Date registrationDate;
+
+    public User() {
+    }
+
+    public User(String username, String password, int age, Date registrationDate) {
+        this.username = username;
+        this.password = password;
+        this.age = age;
+        this.registrationDate = registrationDate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+}
